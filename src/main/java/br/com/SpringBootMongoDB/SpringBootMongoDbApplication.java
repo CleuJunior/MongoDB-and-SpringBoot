6c64e3102cb6a536fe786593a25aa437e1f7ba2d
CleuJunior <cleu.junior@gmail.com>
2022-03-10 02:53:25 -0300

    Making Queries on the Interface

--- src/main/java/br/com/SpringBootMongoDB/SpringBootMongoDbApplication.java
+++ src/main/java/br/com/SpringBootMongoDB/SpringBootMongoDbApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 
 import java.math.BigDecimal;
@@ -16,7 +19,7 @@ public class SpringBootMongoDbApplication {
 	public static void main(String[] args) { SpringApplication.run(SpringBootMongoDbApplication.class, args); }
 
 	@Bean
-	CommandLineRunner runner(StudentRepository studentRepository){
+	CommandLineRunner runner(StudentRepository studentRepository, MongoTemplate mongoTemplate){
 		return args -> {
 			Address address = new Address(
 					"Brasil",
@@ -24,10 +27,11 @@ public class SpringBootMongoDbApplication {
 					"34800000"
 			);
 
+			String email = "cleonildo2.junior@hotmail.com";
 			Student student = new Student(
 					"Cleonildo",
 					"Junior",
-					"cleonildo.junior@hotmail.com",
+					email,
 					Gender.MALE,
 					address,
 					List.of("Matemática", "Geografia", "Física"),
@@ -36,8 +40,29 @@ public class SpringBootMongoDbApplication {
 
 			);
 
-			studentRepository.insert(student);
+
+//			usingMongoTemplateAndQuery(studentRepository, mongoTemplate, email, student);
+
+			studentRepository.findStudentByEmail(email)
+							.ifPresentOrElse(s -> {
+								throw new IllegalStateException(
+										"Foram encontrados mais de um estudante como mesmo email: " + email);
+							}, () -> {
+								studentRepository.insert(student);
+							});
+
 		};
 	}
 
+	private void usingMongoTemplateAndQuery(StudentRepository studentRepository, MongoTemplate mongoTemplate, String email, Student student) {
+		Query query = new Query();
+		query.addCriteria(Criteria.where("email").is(email));
+
+		List<Student> students = mongoTemplate.find(query, Student.class);
+
+		if(students.size() > 0) throw new IllegalStateException(
+				"Foram encontrados mais de um estudante como mesmo email: " + email);
+
+		if(students.isEmpty()) studentRepository.insert(student);
+	}
 }
