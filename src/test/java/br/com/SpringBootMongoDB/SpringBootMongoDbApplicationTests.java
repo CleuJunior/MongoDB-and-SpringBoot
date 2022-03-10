db053029d51fef5d06d415b4f0cb35e326e6d1eb
CleuJunior <cleu.junior@gmail.com>
2022-03-09 21:12:31 -0300

    first commit

---
+++ src/test/java/br/com/SpringBootMongoDB/SpringBootMongoDbApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.SpringBootMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() { }
+
+}
